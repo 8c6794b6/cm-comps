0c2c0bd46e3c9f34a0810bffc936011d8618fdbf
8c6794b6 <8c6794b6@gmail.com>
2014-04-11 16:06:00 +0900

    Added toggle function to JS
    Added haskell side function to call "toggle" function in jQuery.

--- study-for-user-interfaces/src/lib/Sound/Study/ForUserInterfaces/JS.hs
+++ study-for-user-interfaces/src/lib/Sound/Study/ForUserInterfaces/JS.hs
@@ -1,10 +1,11 @@
 module Sound.Study.ForUserInterfaces.JS
     ( getFileName
     , bar
+    , toggle
     ) where
 
 import Control.Applicative ((<$>))
-import Graphics.UI.Threepenny (Element, UI, ffi, callFunction)
+import Graphics.UI.Threepenny (Element, UI, callFunction, ffi, runFunction)
 
 -- | Get file name from \@input type=\"file\"@.
 getFileName :: Element -> UI String
@@ -14,3 +15,7 @@ getFileName el = callFunction $ ffi "getFileName(%1)" el
 -- Arguments in Haskell side are two 'Int's.
 bar :: Int -> Int -> UI Int
 bar x y = read <$> (callFunction $ ffi "bar(%1, %2)" x y)
+
+-- | Toggle display with jQuery's /.toggle()/ function.
+toggle :: Element -> UI ()
+toggle = runFunction . ffi "$(%1).toggle(\"fast\")"
