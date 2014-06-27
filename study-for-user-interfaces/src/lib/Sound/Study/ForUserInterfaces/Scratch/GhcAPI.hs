96660ba7aec640d44f8baac0b518ac2e702a3c5b
8c6794b6 <8c6794b6@gmail.com>
2014-06-27 21:44:57 +0900

    Tidied up a bit

--- study-for-user-interfaces/src/lib/Sound/Study/ForUserInterfaces/Scratch/GhcAPI.hs
+++ study-for-user-interfaces/src/lib/Sound/Study/ForUserInterfaces/Scratch/GhcAPI.hs
@@ -23,11 +23,8 @@ import Sound.OSC (Time, pauseThreadUntil)
 import GHC hiding (Name)
 
 -- import DriverPipeline (link, linkBinary)
-
-
 import DynFlags ( PkgConfRef(..), PackageFlag(..), defaultFatalMessager
                 , defaultFlushOut)
-
 -- import Linker (getHValue, showLinkerState)
 -- import Module (packageIdString, stringToPackageId)
 -- import OccName (mkVarOcc)
@@ -39,24 +36,19 @@ import DynFlags ( PkgConfRef(..), PackageFlag(..), defaultFatalMessager
 import GHC.Paths (libdir)
 
 
--- --------------------------------------------------------------------------
---
--- From "GHC/As a library - Haskell Wiki"
---
--- --------------------------------------------------------------------------
-
-thisModule :: String
-thisModule = moduleOfFunctionName 'thisModule
+-- thisModule :: String
+-- thisModule = moduleOfFunctionName 'thisModule
 
 -- | Evaluate given expression having type @a :: IO ()@.
-eval :: Show a => FilePath -> a -> IO ()
-eval pkgConf expr =
+eval :: Show a => FilePath -> String -> a -> IO ()
+eval pkgConf callerModule expr =
   defaultErrorHandler
    defaultFatalMessager
    defaultFlushOut
    (runGhc
     (Just libdir)
-    (do dflags <- getSessionDynFlags
+    (do let thisModule = callerModule
+        dflags <- getSessionDynFlags
         _ <- setSessionDynFlags
                dflags {verbosity = 0
                       ,extraPkgConfs = (PkgConfFile pkgConf :)
@@ -69,55 +61,42 @@ eval pkgConf expr =
         target <- guessTarget thisModule Nothing
         setTargets [target]
         _ <- load LoadAllTargets
-        setContext [IIModule (mkModuleName thisModule)]
+        setContext [IIDecl (simpleImportDecl (mkModuleName thisModule))]
         result <- dynCompileExpr (show expr)
         liftIO (fromDyn result (putStrLn "Failed"))))
 
--- This works quicker than 'eval', but recursion is hardcoded inside this
--- action, which makes difficult to terminate the forked thread and managing
--- recursive state update.
-callback :: Show a => FilePath -> String -> a -> IO ()
-callback pkgConf sourceModule expr =
-  defaultErrorHandler
-   defaultFatalMessager
-   defaultFlushOut
-   (runGhc
-    (Just libdir)
-    (do setupSession pkgConf sourceModule
-        let err = putStrLn ("callback: Error compiling `" ++ show expr ++ "'")
-            myModuleName = mkModuleName sourceModule
-            go = do _ <- load LoadAllTargets
-                    setContext [IIDecl (simpleImportDecl myModuleName)]
-                    result <- dynCompileExpr (show expr)
-                    liftIO (fromDyn result err)
-                    go
-        go))
-
 -- | Recursively load and evaluate given function name.
-callback' ::
+recurse ::
   FilePath  -- ^ Path to package conf file.
-  -> String -- ^ Module name.
   -> Name   -- ^ Name of function to recurse.
             --
-            -- Expecting an action which returns next state for itself
-            -- to update the state recursively.
+            -- Expecting an IO action having type @(a -> IO a)@, an IO action
+            -- which returns next state for itself to update the state
+            -- recursively.
             --
   -> a      -- ^ Initial argument.
-  -> IO ()
-callback' pkgConf sourceModule expr arg =
+  -> IO b
+recurse pkgConf expr arg =
   defaultErrorHandler
    defaultFatalMessager
    defaultFlushOut
    (runGhc
     (Just libdir)
-    (do setupSession pkgConf sourceModule
-        let myModuleName = mkModuleName sourceModule
-            go x = do _ <- load LoadAllTargets
-                      setContext [IIDecl (simpleImportDecl myModuleName)]
-                      result <- compileExpr (show expr)
-                      x' <- liftIO ((unsafeCoerce result :: a -> IO a) x)
-                      go x'
-        go arg))
+    (do let sourceModule = moduleOfFunctionName expr
+            myModuleName = mkModuleName sourceModule
+            go x fallback =
+               do rflag <- load LoadAllTargets
+                  case rflag of
+                    Failed ->
+                      do x' <- liftIO ((unsafeCoerce fallback :: a -> IO a) x)
+                         go x' fallback
+                    Succeeded ->
+                      do setContext [IIDecl (simpleImportDecl myModuleName)]
+                         result <- compileExpr (show expr)
+                         x' <- liftIO ((unsafeCoerce result :: a -> IO a) x)
+                         go x' result
+        setupSession pkgConf sourceModule
+        go arg (error "recurse: Failed the initial load.")))
 
 withGhc :: FilePath -> String -> Ghc () -> IO ()
 withGhc pkgConf targetSource body =
@@ -145,10 +124,6 @@ setupSession pkgConf targetSource =
                                      ,"src/lib"]}
      target <- guessTarget targetSource Nothing
      setTargets [target]
-     -- rflag <- load LoadAllTargets
-     -- case rflag of
-     --   Succeeded -> return ()
-     --   _         -> error ("Loading " ++ targetSource ++ " failed.")
 
 -- | This works with GHCs running with separate OS process.
 --
@@ -187,8 +162,9 @@ apply func arg fallback =
 apply' :: Name -> a -> Ghc ()
 apply' n arg = apply n arg (\_ -> liftIO (putStrLn "Compilation failed."))
 
-applyAt' :: Time -> Name -> a -> Ghc b
-applyAt' theTime func arg =
+-- applyAt' :: Time -> Name -> a -> Ghc b
+applyAt' :: GhcMonad m => Time -> Name -> t -> m b
+applyAt' scheduledTime func arg =
   do --
      -- Cannot compile expression, try looking up the value of given Name from
      -- current HscEnv to get last result. Use 'ByteCodeLink.lookupName'? Where
@@ -214,6 +190,7 @@ applyAt' theTime func arg =
 
      -- hsc_env <- getSession
      -- hsc_nc <- liftIO (readIORef (hsc_NC hsc_env))
+
      let thisModuleName = mkModuleName (moduleOfFunctionName func)
      --     ocache = nsNames hsc_nc
      --     mdls = [mdl | ms <- hsc_mod_graph hsc_env
@@ -228,13 +205,14 @@ applyAt' theTime func arg =
      -- fallback <- liftIO (getHValue hsc_env name)
      result <- load (LoadUpTo thisModuleName)
      case result of
-       Failed    -> error "applyAt': Failed loading module"
+       Failed    ->
+         error "applyAt': Failed loading module"
          -- do liftIO (pauseThreadUntil theTime)
          --    unsafeCoerce fallback arg
        Succeeded ->
          do setContext [IIModule thisModuleName]
             hvalue <- compileExpr (show func)
-            liftIO (pauseThreadUntil theTime)
+            liftIO (pauseThreadUntil scheduledTime)
             unsafeCoerce hvalue arg
 
 -- applyAt' :: Double -> Name -> a -> Ghc ()
@@ -243,7 +221,7 @@ applyAt theTime func arg =
   do let thisModuleName = mkModuleName (moduleOfFunctionName func)
      result <- load (LoadUpTo thisModuleName)
      case result of
-       Failed    -> -- error "applyAt: Compilation failed."
+       Failed    ->
          do setContext [IIDecl (simpleImportDecl thisModuleName)]
             hvalue <- compileExpr (show func)
             hvalue `seq` liftIO (pauseThreadUntil theTime)
