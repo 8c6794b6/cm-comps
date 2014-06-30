2db7b101d6c27e02d56b4524a19a72eca15ec20d
8c6794b6 <8c6794b6@gmail.com>
2014-06-30 13:27:08 +0900

    Continue the recursion with fallback value
    Added type class to hold fallback value to use in case of compilation
    error. Added newtype having IORef for storing the fallback value, wraps Ghc with
    fallback value. Made the newtype instance of MonadRandom for convenience, though
    not yet sure how much benefits would be found with MonadRandom.
    
    Removed commented out imports.

--- study-for-user-interfaces/src/lib/Sound/Study/ForUserInterfaces/Scratch/GhcAPI.hs
+++ study-for-user-interfaces/src/lib/Sound/Study/ForUserInterfaces/Scratch/GhcAPI.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-|
 Copyright   : 8c6794b6, 2014
 License     : BSD3
@@ -11,33 +12,106 @@ Module to play with ghc package and temporal recursion.
 -}
 module Sound.Study.ForUserInterfaces.Scratch.GhcAPI where
 
-import Control.Monad.IO.Class (liftIO)
-import Data.Dynamic (fromDyn)
-import Data.List (intersperse)
-import Language.Haskell.TH.Syntax (Name)
-import Unsafe.Coerce (unsafeCoerce)
+import           Control.Applicative
+import           Control.Monad (ap, liftM)
+import           Control.Monad.IO.Class (MonadIO(..))
+import           Control.Monad.Random (MonadRandom(..))
+import           Data.Dynamic (fromDyn)
+import           Data.List (intersperse)
+import           Data.IORef
+import           Language.Haskell.TH.Syntax (Name)
+import           Unsafe.Coerce (unsafeCoerce)
 
-import Sound.OSC (Time, pauseThreadUntil)
+import           Sound.OSC (Time, pauseThreadUntil)
 
 -- From "ghc" package
-import GHC hiding (Name)
-
--- import DriverPipeline (link, linkBinary)
-import DynFlags ( PkgConfRef(..), PackageFlag(..), defaultFatalMessager
-                , defaultFlushOut)
--- import Linker (getHValue, showLinkerState)
--- import Module (packageIdString, stringToPackageId)
--- import OccName (mkVarOcc)
--- import HscMain (hscStmt)
--- import HscTypes (HscEnv(..), NameCache(..), emptyHomePackageTable)
--- import IfaceEnv (lookupOrigNameCache)
+import           GHC hiding (Name)
+import           DynFlags
+                  ( HasDynFlags(..), PkgConfRef(..), PackageFlag(..)
+                  , defaultFatalMessager, defaultFlushOut)
+import           Exception (ExceptionMonad(..))
 
 -- From "ghc-paths" package
-import GHC.Paths (libdir)
+import           GHC.Paths (libdir)
 
+data RecEnv t =
+  RecEnv {reHValueRef :: {-# UNPACK  #-} !(IORef HValue)
+         ,reTransport :: {-# UNPACK  #-} !t}
 
--- thisModule :: String
--- thisModule = moduleOfFunctionName 'thisModule
+-- Inspired from GhciMonad.Ghci, see how it's holding Ghc inside.
+-- It is reader Monad using IORef.
+newtype Rec a = Rec {unRec :: IORef HValue -> Ghc a}
+
+instance MonadRandom Rec where
+  getRandom = liftIO getRandom
+  getRandoms = liftIO getRandoms
+  getRandomR = liftIO . getRandomR
+  getRandomRs = liftIO . getRandomRs
+
+-- | Getter and setter fallback value for module reload.
+class HasFallback h where
+  setFallback :: HValue -> h ()
+  getFallback :: h HValue
+
+instance HasFallback Rec where
+  setFallback hvalue = Rec (\ref -> liftIO (writeIORef ref hvalue))
+  getFallback = Rec (liftIO . readIORef)
+
+-- | Setup GHC session and run 'Rec' with initial argument.
+runRecWith ::
+  FilePath        -- ^ Path to package db.
+  -> String       -- ^ Name of target source.
+  -> IO a         -- ^ 'IO' action returning initial argument.
+  -> (a -> Rec b) -- ^ Function taking argument and returns 'Rec' to run.
+  -> IO b
+runRecWith pkgConf targetSource arg frec =
+  do ref <- newIORef (error "runRecWith: fallback not initialized.")
+     defaultErrorHandler
+       defaultFatalMessager
+       defaultFlushOut
+       (runGhc
+          (Just libdir)
+          (do setupSession pkgConf targetSource
+              rflag <- load LoadAllTargets
+              case rflag of
+                Failed    -> error "setupSession: failed loading module."
+                Succeeded -> return ()
+              arg' <- liftIO arg
+              unRec (frec arg') ref))
+
+-- | Like 'runRecWith', but without initial argument.
+runRec :: FilePath -> String -> Rec a -> IO a
+runRec pkgConf targetSource body =
+  runRecWith pkgConf targetSource (return ()) (\_ -> body)
+
+liftGhc :: Ghc a -> Rec a
+liftGhc m = Rec (\_ -> m)
+
+instance Monad Rec where
+  Rec m >>= k = Rec (\r -> m r >>= \a -> unRec (k a) r)
+  return = liftGhc . return
+
+instance Functor Rec where
+  fmap = liftM
+
+instance Applicative Rec where
+  pure = return
+  (<*>) = ap
+
+instance MonadIO Rec where
+  liftIO = liftGhc . liftIO
+
+instance ExceptionMonad Rec where
+  gcatch m h = Rec (\r -> unRec m r `gcatch` (\e -> unRec (h e) r))
+  gmask f = Rec (\r -> gmask (\g -> let f' (Rec m) = Rec (\r' -> g (m r'))
+                                    in  unRec (f f' ) r))
+
+instance HasDynFlags Rec where
+  getDynFlags = liftGhc getDynFlags
+
+instance GhcMonad Rec where
+  getSession = liftGhc getSession
+  setSession = liftGhc . setSession
 
 -- | Evaluate given expression having type @a :: IO ()@.
 eval :: Show a => FilePath -> String -> a -> IO ()
@@ -98,6 +172,10 @@ recurse pkgConf expr arg =
         setupSession pkgConf sourceModule
         go arg (error "recurse: Failed the initial load.")))
 
+-- | Pause thread until given time, then return given value.
+returnAt :: MonadIO m => Time -> a -> m a
+returnAt t a = pauseThreadUntil t >> return a
+
 withGhc :: FilePath -> String -> Ghc () -> IO ()
 withGhc pkgConf targetSource body =
   defaultErrorHandler
@@ -107,7 +185,8 @@ withGhc pkgConf targetSource body =
             (do setupSession pkgConf targetSource
                 body))
 
-setupSession :: FilePath -> String -> Ghc ()
+-- | Setup GHC session with given package conf file and target.
+setupSession :: GhcMonad m => FilePath -> String -> m ()
 setupSession pkgConf targetSource =
   do dflags <- getSessionDynFlags
      _pkgs <- setSessionDynFlags
@@ -125,13 +204,14 @@ setupSession pkgConf targetSource =
      target <- guessTarget targetSource Nothing
      setTargets [target]
 
+
 -- | This works with GHCs running with separate OS process.
 --
 -- Takes TemplateHaskell name of function, possibly updated argument, and an
 -- action to take when failed to reload the module. Only single thread could be
 -- forked at the same time.
 --
-apply :: Name -> a -> (a -> Ghc b) -> Ghc b
+apply :: GhcMonad m => Name -> t -> (t -> m a) -> m a
 apply func arg fallback =
   do --
      -- Tryed 'DriverPipeline.linkBinary'. When linking binary, there will be no
@@ -159,12 +239,11 @@ apply func arg fallback =
             hvalue <- compileExpr (show func)
             unsafeCoerce hvalue arg
 
-apply' :: Name -> a -> Ghc ()
+apply' :: GhcMonad m => Name -> t -> m ()
 apply' n arg = apply n arg (\_ -> liftIO (putStrLn "Compilation failed."))
 
--- applyAt' :: Time -> Name -> a -> Ghc b
-applyAt' :: GhcMonad m => Time -> Name -> t -> m b
-applyAt' scheduledTime func arg =
+applyAt :: (HasFallback m, GhcMonad m) => Time -> Name -> t -> m b
+applyAt scheduledTime func arg =
   do --
      -- Cannot compile expression, try looking up the value of given Name from
      -- current HscEnv to get last result. Use 'ByteCodeLink.lookupName'? Where
@@ -179,18 +258,10 @@ applyAt' scheduledTime func arg =
      -- * Use StateMonad, hold last compiled result. (might work, make newtype
      -- and wrap Ghc in with State Monad, store HValue in the state).
      --
-     -- * Save the source file which compiled successfully, load this if current
-     -- reload iteration has failed. Need to copy the file in every loop
-     -- (workaround).
-     --
      -- * Don't interpret, load object code only. Use does compilation manually
      -- which leads to reloading by looped process (don't understand ghc this
      -- much yet).
      --
-
-     -- hsc_env <- getSession
-     -- hsc_nc <- liftIO (readIORef (hsc_NC hsc_env))
-
      let thisModuleName = mkModuleName (moduleOfFunctionName func)
      --     ocache = nsNames hsc_nc
      --     mdls = [mdl | ms <- hsc_mod_graph hsc_env
@@ -206,30 +277,14 @@ applyAt' scheduledTime func arg =
      result <- load (LoadUpTo thisModuleName)
      case result of
        Failed    ->
-         error "applyAt': Failed loading module"
-         -- do liftIO (pauseThreadUntil theTime)
-         --    unsafeCoerce fallback arg
+         do fallback <- getFallback
+            pauseThreadUntil scheduledTime
+            unsafeCoerce fallback arg
        Succeeded ->
          do setContext [IIModule thisModuleName]
             hvalue <- compileExpr (show func)
-            liftIO (pauseThreadUntil scheduledTime)
-            unsafeCoerce hvalue arg
-
--- applyAt' :: Double -> Name -> a -> Ghc ()
-applyAt :: GhcMonad m => Time -> Name -> t -> m b
-applyAt theTime func arg =
-  do let thisModuleName = mkModuleName (moduleOfFunctionName func)
-     result <- load (LoadUpTo thisModuleName)
-     case result of
-       Failed    ->
-         do setContext [IIDecl (simpleImportDecl thisModuleName)]
-            hvalue <- compileExpr (show func)
-            hvalue `seq` liftIO (pauseThreadUntil theTime)
-            unsafeCoerce hvalue arg
-       Succeeded ->
-         do setContext [IIModule thisModuleName]
-            hvalue <- compileExpr (show func)
-            hvalue `seq` liftIO (pauseThreadUntil theTime)
+            setFallback hvalue
+            pauseThreadUntil scheduledTime
             unsafeCoerce hvalue arg
 
 moduleOfFunctionName :: Name -> String
