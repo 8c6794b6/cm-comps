365a04127b45d77909748d1c6b7178d08842752a
8c6794b6 <8c6794b6@gmail.com>
2014-08-13 23:21:36 +0900

    Added Data and Typeable orphan

--- hsc3-orphan/src/lib/Sound/SC3/Orphan.hs
+++ hsc3-orphan/src/lib/Sound/SC3/Orphan.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
@@ -19,22 +20,33 @@ import Sound.SC3
 import Sound.SC3.UGen.MCE
 
 import GHC.Generics (Generic)
+import Data.Data (Data, Typeable)
 import Data.Hashable (Hashable)
 
 #define DERIVE_1(ty) \
 deriving instance Generic ty; \
+deriving instance Typeable ty; \
+deriving instance Data ty; \
 instance Hashable ty;
 
-DERIVE_1(UGenId)
-DERIVE_1(Special)
-DERIVE_1(Proxy)
-DERIVE_1(Primitive)
-DERIVE_1(MRG)
-DERIVE_1(Label)
-DERIVE_1(Rate)
-DERIVE_1(Control)
+DERIVE_1(AddAction)
 DERIVE_1(Constant)
+DERIVE_1(Control)
+DERIVE_1(FromPort)
+DERIVE_1(Graph)
+DERIVE_1(KType)
+DERIVE_1(Label)
+DERIVE_1(MRG)
+DERIVE_1(Node)
+DERIVE_1(Primitive)
+DERIVE_1(Proxy)
+DERIVE_1(Rate)
+DERIVE_1(Special)
+DERIVE_1(Synthdef)
 DERIVE_1(UGen)
+DERIVE_1(UGenId)
 
 deriving instance Generic (MCE a)
+deriving instance Typeable MCE
+deriving instance Data a => Data (MCE a)
 instance Hashable a => Hashable (MCE a)
